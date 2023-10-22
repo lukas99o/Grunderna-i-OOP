51a408193856355c51622becb4170344641e7685
lukas99o <lukas99o@hotmail.com>
2023-10-22 09:43:28 +0200

    Add project files.

---
+++ Grunderna i OOP/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grunderna_i_OOP
+{
+    internal class Circle
+    {
+        private double _radie;
+
+        private string _name;
+        
+        public Circle(double radie, string name)
+        {
+            _radie = radie;
+            _name = name;
+            GetArea();
+        }
+
+        public void GetArea()
+        {
+            Console.WriteLine("----------");
+            Console.WriteLine(_name + ":");
+            Console.WriteLine();
+            Console.WriteLine("Radie: " + _radie);
+            double area = _radie * _radie * Math.PI;
+            Console.WriteLine("Area: " + area);       
+        }   
+    }
+}
---
+++ Grunderna i OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Grunderna_i_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle1 = new Circle(5, "Circle 1");
+            Circle myCircle2 = new Circle(6, "Circle 2");
+            Console.WriteLine("----------");
+
+            Console.WriteLine();
+            Console.Write("Press Any Key To Exit The Program: ");
+            Console.ReadKey();
+        }
+    }
+}
